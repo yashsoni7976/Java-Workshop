3e8d33ce2713b7870ef23b7d536c8a6ff1e40734
yashsoni7976 <94311115+yashsoni7976@users.noreply.github.com>
2022-11-04 16:08:30 +0530

    Create ArmstrongUsingFunction.java

---
+++ ArmstrongUsingFunction.java
@@ -0,0 +1,41 @@
+import java.lang.Math;
+import java.util.Scanner;
+class ArmstrongUsingFunction
+{
+    int count=0;
+    void findNoOfDigit(int num)
+    {
+        while(num!=0)
+        {
+            int temp=num%10;
+            count++;
+            num/=10;
+        }
+    }
+    int findingSumofDigit(int num)
+    {
+        int sum=0;
+        while(num!=0)
+        {
+            int temp=num%10;
+            sum+=(Math.pow(temp,count));
+            num/=10;
+        }
+        return sum;
+    }
+
+    public static void main(String arg[])
+    {
+        ArmstrongUsingFunction f1=new ArmstrongUsingFunction();
+        Scanner sc=new Scanner(System.in);
+        int numb;
+        System.out.print("Enter number : ");
+        numb=sc.nextInt();
+        f1.findNoOfDigit(numb);
+        int res=f1.findingSumofDigit(numb);
+        if(res==numb)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
